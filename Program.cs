c36127c0e0eaac2761a04b24af7b6c638b5ef414
ReiBalzac <esyalavi@gmail.com>
2021-09-29 22:56:06 +0200

    сделала красивее

--- Program.cs
+++ Program.cs
@@ -5,35 +5,29 @@ namespace Сортировка
     class Program
     {
         static void Main(string[] args)
-        {
-            
+        {            
             Console.WriteLine("How many digits will there be in the string?");
             var kolichestvoPusirkov = int.Parse(Console.ReadLine());
             int[] mas = new int[kolichestvoPusirkov];
-
             Console.WriteLine("Enter max number");
             var maxSide = int.Parse(Console.ReadLine());
-            Random rand = new Random();
-            
+            Random rand = new Random();            
             for (int i = 0; i < kolichestvoPusirkov; i++)
             {
-
-                mas[i] = rand.Next(1, maxSide + 1);
-               
+                mas[i] = rand.Next(1, maxSide + 1);               
             }
-
-
             for (int i = 0; i < kolichestvoPusirkov; i++)
-
             {
-
                 {
-                    Console.Write("{0 }, ", mas[i]);
-                    
+                    Console.Write("{0 }, ", mas[i]);                   
                 }
             }
-            
-          
+            {
+                Console.WriteLine();
+            }
+            {
+                Console.WriteLine("Sorted array:");
+            }
             for (int i = 0; i < kolichestvoPusirkov; i++)
            {
                 for (int j = i + 1; j < kolichestvoPusirkov; j++)
@@ -44,16 +38,12 @@ namespace Сортировка
                         int t = mas[i];
                         mas[i] = mas[j];
                         mas[j] = t;
-                    }
-                     
+                    }                     
                 }
-                Console.Write("{0 }, ", mas[i]);             
-
-
-
+                Console.Write("{0 }, ", mas[i]);
 
             }
-
+          
         }
     }
 }
